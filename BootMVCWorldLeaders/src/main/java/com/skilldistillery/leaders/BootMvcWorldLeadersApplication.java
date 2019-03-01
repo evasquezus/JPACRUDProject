328bf4a0c042fa6590896c7458b0bf5e5516495f
Edwin <giolambo@aol.com>
2019-03-01 15:44:08 -0700

    Start Boot Project renamed jpa packages

---
+++ BootMVCWorldLeaders/src/main/java/com/skilldistillery/leaders/BootMvcWorldLeadersApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.leaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcWorldLeadersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcWorldLeadersApplication.class, args);
+	}
+
+}
